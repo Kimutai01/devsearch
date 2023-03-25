69e80c76829df1db3ab403498ac239f599173255
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-03-25 16:09:08 +0300

    Added controllers

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,7 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  has_one_attached :avatar
+  has_many :messages, dependent: :destroy
+  has_many :skills, dependent: :destroy
+  has_many :projects, dependent: :destroy
 end
